94f8ab8ff50a0931266da68ccbc3739bce6a120c
sushriss <71025929+sushriss@users.noreply.github.com>
2020-09-09 20:32:56 +0530

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,14 @@
+
+num1=float(input("enter a number: "))
+op=input("enter operator: ")
+num2=float(input("enter second number: "))
+if op == "+":
+    print(num1+num2)
+elif op == "*":
+    print(num1*num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "-":
+    print(num1-num2)
+else:
+    print("invalid operator")
